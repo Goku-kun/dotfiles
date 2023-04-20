94553c35d1db918c7e5e0d41ab730be3d0892706
Goku-kun <jethvadharmarajsinh@gmail.com>
2023-04-20 09:49:50 +0530

    Fix: LSP Error floating buffer colors

--- nvim/refactored-linux-nvim/nvim/after/plugin/colors.lua
+++ nvim/refactored-linux-nvim/nvim/after/plugin/colors.lua
@@ -9,4 +9,7 @@ vim.cmd('hi EndOfBuffer guibg=NONE ctermbg=NONE')
 -- Git Fugitive Diff colors
 vim.cmd('hi diffAdded ctermfg=black ctermbg=green guifg=black guibg=lightgreen')
 
+-- LSP Floating window error colors
+vim.cmd('hi DiagnosticFloatingError guifg=#c13e46 gui=bold guibg=NONE')
+vim.cmd('highlight FloatBorder guibg=NONE')
 
