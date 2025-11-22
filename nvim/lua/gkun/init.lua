f10e136937a994be9506e8ca6040a4590118cadf
Dharmarajsinh Jethva <jethvadharmarajsinh@gmail.com>
2025-11-22 21:40:32 +0530

    Set leader and local leader keys in init.lua

--- nvim/lua/gkun/init.lua
+++ nvim/lua/gkun/init.lua
@@ -1,3 +1,8 @@
-require("gkun.remap")
+-- Set leader key FIRST, before any keymaps are defined
+-- <Leader> is replaced by mapleader value at mapping definition time
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
 require("gkun.set")
+require("gkun.remap")
 require("gkun.lazy")
